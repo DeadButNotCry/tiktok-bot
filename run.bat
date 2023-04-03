1ff46ee3fc821d84b4972fe95cafbc0bf2f965b0
deadbutnotcry <deadbutnotcry@gmail.com>
2023-04-04 00:21:20 +0300

    remove credentials

--- run.bat
+++ run.bat
@@ -1,6 +1,5 @@
-set "BOT_TOKEN=5945815849:AAEz1DnAUCW4G2DGKacwdHvy602T-7l8cZ4" 
-set "GROUP_ID=-1001923600726" 
+set "BOT_TOKEN=" 
+set "GROUP_ID=" 
 
-echo "$BOT_TOKEN $GROUP_ID"
 go build -o .\build\main.exe  .\cmd\main.go
 .\build\main.exe
